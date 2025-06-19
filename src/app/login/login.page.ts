81c737b717915c286db1e8115efc913e7504fff9
roodiaz <roabigail.diaz@gmail.com>
2025-06-18 21:36:27 -0300

    se sube formulario reactivo para login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { 
-  IonContent, 
-  IonItem, 
-  IonLabel, 
-  IonInput, 
-  IonButton, 
-  IonText 
-} from '@ionic/angular/standalone';
+import { IonContent, IonItem, IonLabel, IonInput, IonButton, IonText, ToastController } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
@@ -17,8 +10,8 @@ import {
   styleUrls: ['./login.page.scss'],
   standalone: true,
   imports: [
+    ReactiveFormsModule,
     CommonModule,
-    FormsModule,
     IonContent,
     IonItem,
     IonLabel,
@@ -27,20 +20,65 @@ import {
     IonText
   ]
 })
-export class LoginPage implements OnInit {
-  constructor(private router: Router) { }
-  
-  ngOnInit() {}
 
-  // Variables para el formulario
-  dni: number | null = null;
+export class LoginPage implements OnInit {
+
+  formulario: FormGroup;
+  dni: number = 0;
   password: string = '';
 
+  constructor(private router: Router, private fb: FormBuilder, private toastController: ToastController) {
+    this.formulario = this.fb.group({
+      dni: [null, [
+        Validators.required, 
+        Validators.min(10000000), 
+        Validators.max(99999999)
+      ]],
+      password: ['', [
+        Validators.required, 
+        Validators.minLength(8)
+      ]]
+    });
+  }
+
+  // toma y setea los valores ingresados en los inputs
+  ngOnInit(): void {
+    this.formulario.get('dni')?.valueChanges.subscribe((value) => {
+      this.dni = value;
+    });
+    this.formulario.get('password')?.valueChanges.subscribe((value) => {
+      this.password = value;
+    });
+  }
+
   login() {
-    if (this.dni === 12345678 && this.password === '12345678') {
+
+    const dniIngresado = this.formulario.value.dni;
+    const passIngresada = this.formulario.value.password;
+  
+    const dniCorrecto = 12345678;
+    const passCorrecta = '12345678';
+  
+    if (dniIngresado === dniCorrecto && passIngresada === passCorrecta) {
       this.router.navigate(['/home']);
-    } else {
-      alert('DNI o contraseña incorrectos');
+    } else if (dniIngresado !== dniCorrecto && passIngresada !== passCorrecta) {
+      this.toast('El DNI y la contraseña son incorrectos');
+    } else if (dniIngresado !== dniCorrecto) {
+      this.toast('El DNI es incorrecto');
+    } else if (passIngresada !== passCorrecta) {
+      this.toast('La contraseña es incorrecta');
     }
+  }
+  
+
+  // metodo para mostrar msj emergente temporal
+  async toast(mensaje: string) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 2000,
+      position: 'bottom',
+      cssClass: 'custom-toast',
+    });
+    toast.present();
   }
 }
